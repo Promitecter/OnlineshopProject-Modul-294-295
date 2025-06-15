1cd13f54dda276459a8392393ec8226243c4c5b0
Roman Flükiger <roman.fluekiger@gmail.com>
2025-06-15 21:06:41 +0200

    prepared spring boot

---
+++ backend/src/test/java/ch/wiss/onlineshop/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.wiss.onlineshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
