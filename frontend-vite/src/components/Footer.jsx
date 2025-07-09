3fba8c984709d4f28c2e94e1580367d23e6b3502
Roman Flükiger <roman.fluekiger@gmail.com>
2025-07-09 17:29:11 +0200

    FooterLink edited

--- frontend-vite/src/components/Footer.jsx
+++ frontend-vite/src/components/Footer.jsx
@@ -3,7 +3,7 @@ import '../styles/components/Footer.css';
 export default function Footer() {
     return (
         <footer className="main-footer">
-        <p>© Onlineshopfactory.ch / Onlineshop-Projekt mit JAVA und REACT</p>
+        <p><a href="https://onlineshopfactory.ch" target="_blank">Onlineshopfactory.ch</a> / Onlineshop-Projekt mit JAVA und REACT</p>
         </footer>
     );
 }
